59fc39d15e2abf12ebf654d7d037de257546a462
chun man poon <chunman906@hotmail.com>
2022-08-04 19:50:50 +0100

    migrate model

--- db/migrate/20220804184840_create_invoices.rb
+++ db/migrate/20220804184840_create_invoices.rb
@@ -1,8 +1,7 @@
 class CreateInvoices < ActiveRecord::Migration[7.0]
   def change
     create_table :invoices do |t|
-      t.integer :id
-      t.string :descrption
+      t.string :description
       t.float :amount
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_131140) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_184840) do
+  create_table "invoices", force: :cascade do |t|
+    t.string "description"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "payments", force: :cascade do |t|
     t.integer "paymentNumber"
     t.float "amount"
